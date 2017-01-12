1807baf25a47c8e180ed23e886db4a1806cd683b
Hayden Schiff <haydenschiff@gmail.com>
2017-01-11 21:28:52 -0500

    added WebBundle with new home page

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
             new AppBundle\AppBundle(),
+            new WebBundle\WebBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
